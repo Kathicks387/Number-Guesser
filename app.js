3f41dfca778f87169cad7262519e13273502cf7f
Can Do <Katrinarhicks@gmail.com>
2020-12-04 11:42:08 -0500

    completed js codes and add css

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 // Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max);
     guessesLeft = 3;
 
 // UI Elements
@@ -25,13 +25,20 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+// Play again event listener
+game.addEventListener('mousedown', function(e){
+  if(e.target.className === 'play-again') {
+    window.location.reload();
+  }
+})
+
 //Listen for guess
 guessBtn.addEventListener('click', function() {
  let guess = parseInt(guessInput.value);
  
  // Validate
  if(isNaN(guess) || guess < min || guess > max){
-  setMessage(`Please enter a number between ${min} and ${max}`, 'red');
+  setMessage(`Please enter a number between ${min} and ${max}`, 'darkred');
 }
 
 //Check if won
@@ -49,13 +56,13 @@ if(guess === winningNum) {
    // Game continues - answer wrong
 
       // Change border color
-      guessInput.style.borderColor = 'red';
+      guessInput.style.borderColor = 'darkred';
 
       // Clear Input
       guessInput.value = '';
 
       // Tell user its the wrong number
-      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'darkred');
   
   }
 }
@@ -64,7 +71,7 @@ if(guess === winningNum) {
 // Game over
 function gameOver(won, msg) {
   let color;
-  won === true ? color = 'green' : color = 'red';
+  won === true ? color = 'darkgreen' : color = 'darkred';
 
   // Disable input
   guessInput.disabled = true;
@@ -74,6 +81,15 @@ function gameOver(won, msg) {
   message.style.color = color;
   // Set message
   setMessage(msg);
+
+  // Play Again?
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max){
+   return(Math.floor(Math.random()*(max-min+1)+min));
 }
 
 // Set message
